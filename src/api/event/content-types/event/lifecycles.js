4c2d5fd91430be4a21c2dababa62104b4d319c11
Yeon Kim <awyeon@gmail.com>
2022-09-15 16:55:08 +0800

    custom user events endpoint

--- src/api/event/content-types/event/lifecycles.js
+++ src/api/event/content-types/event/lifecycles.js
@@ -1,36 +1,19 @@
-// const slugify = require("slugify");
-
-// module.exports = {
-//   beforeCreate(createdEvent) {
-//     const { data } = createdEvent.params;
-
-//     if (data.name) {
-//       data.slug = slugify(data.name, { lower: true });
-//     }
-//   },
-
-//   beforeUpdate(updatedEvent) {
-//     const { data } = updatedEvent.params;
-
-//     if (data.name) {
-//       data.slug = slugify(data.name, { lower: true });
-//     }
-//   },
-// };
-
 const slugify = require("slugify");
 
 module.exports = {
-  beforeCreate(event) {
-    setSlug(event.params.data);
-  },
-  beforeUpdate(event) {
-    setSlug(event.params.data);
-  },
-};
+  beforeCreate(createdEvent) {
+    const { data } = createdEvent.params;
 
-const setSlug = (data) => {
-  if (data.name) {
-    data.slug = slugify(data.name.toLowerCase());
-  }
+    if (data.name) {
+      data.slug = slugify(data.name, { lower: true });
+    }
+  },
+
+  beforeUpdate(updatedEvent) {
+    const { data } = updatedEvent.params;
+
+    if (data.name) {
+      data.slug = slugify(data.name, { lower: true });
+    }
+  },
 };
